fa0f00b5f0ea32164dfe2e2d400be9251f7bde4f
bigmother <794720383@qq.com>
2018-09-18 13:50:18 +0800

    冒泡排序算法

--- src/sort/Bubble.java
+++ src/sort/Bubble.java
@@ -1,9 +1,37 @@
 package sort;
 
+import java.util.Arrays;
+import java.util.Random;
+
 /**
  * author: xuzhemin
  * 2018/9/18 11:45
  * 冒泡排序
+ * 时间复杂度 log(n^2)
  */
 public class Bubble {
+
+    public static void main(String[] args){
+        int len = 10;
+        int[] arr = new int[len];
+        for (int i=0;i<len;i++){
+            arr[i]=new Random().nextInt(100);
+        }
+        System.out.println("before:"+Arrays.toString(arr));
+        System.out.println("after"+Arrays.toString(sortASC(arr)));
+    }
+
+    public static int[] sortASC(int[] arr){
+        int len = arr.length;
+        for (int i=0;i<len;i++){
+            for (int j=i;j<len;j++){
+                if (arr[i]>arr[j]){
+                    int tmp = arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=tmp;
+                }
+            }
+        }
+        return arr;
+    }
 }
